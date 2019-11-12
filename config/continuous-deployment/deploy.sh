b41f247d17a7df94c30343b4cbd51f87d5996ef3
Mareak <mareak@mareak.com>
2019-11-12 16:00:36 +0100

    typo in dockerfile

--- config/continuous-deployment/deploy.sh
+++ config/continuous-deployment/deploy.sh
@@ -3,6 +3,7 @@
 export() {
 	az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 	az account set --subscription=$ARM_SUBSCRIPTION_ID
+    cd ./config/continuous-deployment/
 	terraform init
     TAG='v1.'$((${TAG:3}+1))
 	terraform apply -var TAG=$TAG -var AZURE_USERNAME_REG=$AZURE_USERNAME_REG -var AZURE_PASSWORD_REG=$AZURE_PASSWORD_REG -var AZURE_SERVER_REG=$AZURE_SERVER_REG -auto-approve
